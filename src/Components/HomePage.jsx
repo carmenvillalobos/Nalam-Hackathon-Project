6a18fe702b97173c75ea5f16e6e6a26c1a69d531
carmenvillalobos <carmvilla15@gmail.com>
2023-11-11 18:07:49 -0800

    added link pages

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <div>
+        <nav>
+            <ul>
+                {/* <li><Link to="/">Home</Link></li>
+                <li><Link to="/donate">Donate</Link></li> */}
+            </ul>
+        </nav>
+
       <h1>Changing Lives One Girl At A Time</h1>
       <p>Website under construction. Relaunch in early November. Until the relaunch, please note that all values are conceptual and non-binding. Any questions or inquiries, please contact ulrichkeller@gmail.com. Thanks for your patience :)</p>
 
-      <a className='btn btn-primary'>Donate</a>
+      {/* <Link to="/donate" className='btn btn-primary'>Donate</Link> */}
     </div>
   );
 };
