d36da92bc52eb83e234e2e5696a4e7db0971ae69
ArielManalo14 <91509709+ArielManalo14@users.noreply.github.com>
2023-11-11 17:47:11 -0800

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './Components/HomePage';
+import DonatePage from './Components/DonatePage';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <HomePage />
+        
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +25,12 @@ function App() {
         >
           Learn React
         </a>
+ 
+        <Router>
+          <Link to="/DonatePage">DonatePage</Link>
+        </Router>
+ 
+ 
       </header>
     </div>
   );
---
+++ src/Components/DonatePage.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import './DonatePage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const DonatePage = () => {
+  return (
+    <div>
+      <h1>Hello, I'm a React Component!</h1>
+
+      <div>
+			<link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css"/>
+<div class="container bootstrap snippets bootdey">
+    <hr/>
+    <div class="row content">
+		<div class="col-lg-10 col-lg-offset-1">
+			<div class="col-sm-4 text-center">
+				<div class="plan">
+					<p class="icon bronze">
+						<i class="fa fa-tablet fa-fw"></i>
+					</p>
+					<h2 class="bronze-text">Bronce</h2>
+					<p class="bronze-text price">$12</p>
+					<p>Single License</p>
+					<p>One Website</p>
+					<p>Basic Support</p>
+					<a class="btn btn-pricing" href="#/" target="_blank">Order Now</a>
+				</div>
+			</div>
+		
+			<div class="col-sm-4 text-center">
+				<div class="plan featured">
+					<p class="icon silver">
+						<i class="fa fa-laptop fa-fw"></i>
+					</p>
+					<h2>Silver</h2>
+					<p class="price">$24</p>
+					<p>Multiple Licenses</p>
+					<p>Unlimited Websites</p>
+					<p>Great Support</p>
+					<a class="btn btn-pricing" href="#/" target="_blank">Order Now</a>
+				</div>
+			</div>
+			
+			<div class="col-sm-4 text-center">
+				<div class="plan">
+					<p class="icon gold">
+						<i class="fa fa-desktop fa-fw"></i>
+					</p>
+					<h2 class="gold-text">Gold</h2>
+					<p class="gold-text price">$399</p>
+					<p>Extended License</p>
+					<p>Unlimited Websites</p>
+					<p>Premium Support</p>
+					<a class="btn btn-pricing" href="#/" target="_blank">Order Now</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+		</div>
+
+
+    </div>
+    
+  );
+};
+
+export default DonatePage;
