ffc3f76283d0f505ce9553faffda412e6ecc32bb
carmenvillalobos <carmvilla15@gmail.com>
2023-11-11 16:24:51 -0800

    added a homepage component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './Components/HomePage';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <HomePage />
         <a
           className="App-link"
           href="https://reactjs.org"
